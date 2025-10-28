f4dea157e53c100ff4055d119aed0a78ae2d18f3
Bernardo Vilela <bernardosanchesvilela@gmail.com>
2025-10-28 12:44:56 +0000

    Adicionar rota da página ModuleDetail no configurador de páginas

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,29 +1,23 @@
-import Dashboard from './pages/Dashboard';
-import Projects from './pages/Projects';
-import Tasks from './pages/Tasks';
-import ProjectDetail from './pages/ProjectDetail';
-import Backlog from './pages/Backlog';
-import Companies from './pages/Companies';
-import Opportunities from './pages/Opportunities';
-import Lancamentos from './pages/Lancamentos';
-import ModuleDetail from './pages/ModuleDetail';
-import Layout from './Layout.jsx';
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Tasks from "./pages/Tasks";
+import Backlog from "./pages/Backlog";
+import Companies from "./pages/Companies";
+import ProjectDetail from "./pages/ProjectDetail";
+import Opportunities from "./pages/Opportunities";
+import Lancamentos from "./pages/Lancamentos";
+import ModuleDetail from "./pages/ModuleDetail";
 
+const pages = [
+  { name: "Dashboard", component: Dashboard },
+  { name: "Projects", component: Projects },
+  { name: "Tasks", component: Tasks },
+  { name: "Backlog", component: Backlog },
+  { name: "Companies", component: Companies },
+  { name: "ProjectDetail", component: ProjectDetail },
+  { name: "Opportunities", component: Opportunities },
+  { name: "Lancamentos", component: Lancamentos },
+  { name: "ModuleDetail", component: ModuleDetail },
+];
 
-export const PAGES = {
-    "Dashboard": Dashboard,
-    "Projects": Projects,
-    "Tasks": Tasks,
-    "ProjectDetail": ProjectDetail,
-    "Backlog": Backlog,
-    "Companies": Companies,
-    "Opportunities": Opportunities,
-    "Lancamentos": Lancamentos,
-    "ModuleDetail": ModuleDetail,
-}
-
-export const pagesConfig = {
-    mainPage: "Dashboard",
-    Pages: PAGES,
-    Layout: Layout,
-};
+export default pages;
