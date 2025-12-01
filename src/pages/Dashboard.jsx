9cf19cb879daeb7efe01f1ce0b9f83393a65b89b
Bernardo Vilela <bernardosanchesvilela@gmail.com>
2025-12-01 16:09:46 +0000

    Manual code change

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -110,7 +110,7 @@ export default function Dashboard() {
             <Link to={createPageUrl("Projects")} className="flex-1 sm:flex-initial">
               <Button className="bg-slate-700 text-[#131A20] px-4 py-2 text-sm font-medium rounded-lg inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 w-full sm:w-auto dark:text-white hover:bg-white/80 shadow-md md:px-6 h-10 md:h-11 border border-[#EAEAEA] dark:border-[#30363d]">
                 <Plus className="w-4 h-4 md:w-5 md:h-5 mr-2" />
-                <span className="text-sm md:text-base font-medium">Novo Projeto</span>
+                <span className="bg-transparent text-sm font-medium md:text-base">Novo Projeto</span>
               </Button>
             </Link>
             <Link to={createPageUrl("Tasks")} className="flex-1 sm:flex-initial">
