5393293155e2ff5177eeea3ab16b70f6c01920d3
Bernardo Vilela <bernardosanchesvilela@gmail.com>
2026-03-10 13:57:01 +0000

    File changes

--- src/components/client-portal/ClientAccessManager.jsx
+++ src/components/client-portal/ClientAccessManager.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { sendClientInvite } from "@/functions/sendClientInvite";
+import { base44 as base44sdk } from "@/api/base44Client";
+const sendClientInvite = (payload) => base44sdk.asServiceRole.functions.invoke("sendClientInvite", payload);
 import {
   Plus, Trash2, UserCheck, UserX, Shield, RefreshCw,
   ChevronDown, ChevronUp, Edit2, Save, X, Send, Copy, CheckCheck,
