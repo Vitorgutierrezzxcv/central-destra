7cb4834ddcf8beba415f6c6216773dcbf251c219
Bernardo Vilela <bernardosanchesvilela@gmail.com>
2026-03-10 03:16:22 +0000

    File changes

--- src/pages/ClientPortalDashboard.jsx
+++ src/pages/ClientPortalDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { base44 } from "@/api/base44Client";
 import { Building2, TrendingUp, CheckCircle2, Clock, Calendar, AlertCircle, ArrowRight, Star, Loader2 } from "lucide-react";
